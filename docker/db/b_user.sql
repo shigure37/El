bd0272c6ac4a183567d9a146a553f6bdbad5522c
mavinyeh <mavinyeh@gmail.com>
2018-05-16 16:28:44 +0000

    11

--- docker/db/b_user.sql
+++ docker/db/b_user.sql
@@ -1,3 +1,3 @@
-CREATE USER 'shigure' IDENTIFIED BY 'GuoKe618';
-GRANT ALL PRIVILEGES ON stockdb.* To 'shigure'; 
+CREATE USER 'shigure'@'%' IDENTIFIED BY 'GuoKe618';
+GRANT ALL PRIVILEGES ON stockdb.* To 'shigure'@'%'; 
 
