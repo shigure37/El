3c8c478f6026cbd4f5f5e41f0eb1ab054527a3a0
shigure37 <mavinyeh@gmail.com>
2018-05-17 10:58:47 +0800

    aaa

--- scripts/getstockinfo.py
+++ scripts/getstockinfo.py
@@ -12,7 +12,15 @@ if __name__ == "__main__":
     #print(type(df))
     db = pymysql.connect("127.0.01", "shigure", "GuoKe618", "stockdb")
     cursor = db.cursor()
-    cursor.execute("SELECT VERSION()")
+    for index, row in df.iterrows():
+        insert_sql = """
+                    INSERT INTO tb_stockbaseinfo(code, name)
+                    values (%(code)s, %(name)s)
+                    """ % {'code':row['code'], 'name':row['name']}
+    try:
+        cursor.execute(insert_sql)
+        db.commit()
+    except:
+        db.rollback()
     data = cursor.fetchone()
-    print("database version:%s" % data)
     db.close()
---
+++ test.py
@@ -0,0 +1,13 @@
+# -*- encoding=utf-8 -*- #
+#===============================================================
+#   FileName    : test.py
+#   Author      : mavinyeh
+#   Date        : 2018-05-17
+#   Description : 
+#================================================================
+
+test = """
+      thi sis s a  ljdsal %(CODE)s
+      value:%(VALUE)d
+      """ % {'CODE':'++++', 'VALUE':2222}
+print (test)
