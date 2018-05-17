8ff9b4fe28d87ca0b8137399639c797f950ff193
shigure37 <mavinyeh@gmail.com>
2018-05-17 08:30:36 +0800

    aaa

--- scripts/getstockinfo.py
+++ scripts/getstockinfo.py
@@ -6,7 +6,10 @@
 #   Description : 
 #================================================================
 import tushare as ts
+import pymysql 
 if __name__ == "__main__":
     df = ts.get_stock_basics()
-    print(type(df))
-
+    #print(type(df))
+    db = pymysql.connect("localhost", "shigure", "GuoKe618", "stockdb")
+    cursor = db.cursor()
+    cursor.execute("SELECT VERSION()")
