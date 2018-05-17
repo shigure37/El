fce886c55108216508a00d04b3dd41c169c3ae93
shigure37 <mavinyeh@gmail.com>
2018-05-17 10:03:08 +0800

    aas

--- scripts/getstockinfo.py
+++ scripts/getstockinfo.py
@@ -10,9 +10,9 @@ import pymysql
 if __name__ == "__main__":
     df = ts.get_stock_basics()
     #print(type(df))
-    db = pymysql.connect("localhost", "shigure", "GuoKe618", "stockdb")
+    db = pymysql.connect("127.0.01", "shigure", "GuoKe618", "stockdb")
     cursor = db.cursor()
     cursor.execute("SELECT VERSION()")
-    data = curosor.fetchone()
+    data = cursor.fetchone()
     print("database version:%s" % data)
     db.close()
