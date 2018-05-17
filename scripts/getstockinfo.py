de91f8e258552623acb0b64f42b8ca80e49ee739
shigure37 <mavinyeh@gmail.com>
2018-05-17 16:07:36 +0800

    sad

--- scripts/getstockinfo.py
+++ scripts/getstockinfo.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
     cursor = db.cursor()
     for index, row in df.iterrows():
         insert_sql = """
-                    INSERT INTO tb_test(code, name)
-                    values ('%(code)s', '%(name)s')
-                    """ % {'code':index, 'name':row['name']}
+                    INSERT INTO tb_stock_basic_info(code, name, industry, area, pe, outstanding, totals, totalAssets, liquidAssets, fixedAssets, reserved, reservedPerShared, esp, bvps, pb, timeToMarket, undp, perundp, rev, profit, gpr, npr, hoders, concept)
+                    values ('%(code)s', '%(name)s, '%(area)s', %(pe)d, %(outstanding)d, %(totals), %(totalAssets)d, %(liquidAssets)d, %(fixedAssets)d, %(reserved)d, %(reservedPerShared)d, %(esp)d, %(bvps)d, %(pb)d, %(timeToMarket)d, %(undp)d, %(perundp)d, %(rev)d, %(profit)d, %(gpr)d, %(npr)d, %(hoders)d, '%()s' )
+                    """ % {'code':index, 'name':row['name'], 'industry':row['industry'], 'area':row['area'], 'pe':row['pe'], 'outstanding':row['outstanding'], 'totals':row['totals'], 'totalAssets':row['totalAssets'], 'liquidAssets':row['liquidAssets'], 'fixedAssets':row['fixedAssets'], 'reserved':row['reserved'], 'reservedPerShared':row['reservedPerShared'], 'esp':row['esp'], 'bvps':row['bvps'], 'pb':row['pb'], 'timeToMarket':row['timeToMarker'], 'undp':row['undp'], 'perundp':row['perundp'], 'rev':row['rev'], 'profit':row['profit'], 'gpr':row['gpr'], 'npr':row['npr'], 'hoders':row['hoders'], 'concept':'NULL'}
         try:
             cursor.execute(insert_sql)
             db.commit()
